5644833f16638e62a57753644037f941e8ab3515
Fernando Salazar <fernandsalazar3@gmail.com>
2019-03-07 23:43:13 -0300

    init project. setup apollo client. connect backend with client. basic routing react router v4 airbnb eslint config added

---
+++ src/containers/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const allUsersQuery = gql`
+  {
+    allUsers {
+      _id
+      username
+    }
+  }
+`;
+
+const Home = ({ data: { loading, allUsers } }) => loading ? <span>asdas</span> : allUsers.map(user => (
+        <h1 key={user._id}>
+          {user._id}
+          /
+          {user.username}
+        </h1>
+      ))
+
+export default graphql(allUsersQuery)(Home);
